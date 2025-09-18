7a6e60b6c59c47db5785a837c7164dbc760d9ea0
marcinwizgird <marcin.wizgird@gsmsil.vom>
2025-09-18 22:04:44 +0200

    Project - Phase 1 - RoutingAgent implemented

--- project/starter/phase_1/routing_agent.py
+++ project/starter/phase_1/routing_agent.py
@@ -2,40 +2,58 @@
 # TODO: 1 - Import the KnowledgeAugmentedPromptAgent and RoutingAgent
 import os
 from dotenv import load_dotenv
+from workflow_agents.base_agents import KnowledgeAugmentedPromptAgent, RoutingAgent
 
 # Load environment variables from .env file
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key =  "voc-744678734159874169548468bddf97879340.20823146"
 
 persona = "You are a college professor"
 
 knowledge = "You know everything about Texas"
 # TODO: 2 - Define the Texas Knowledge Augmented Prompt Agent
+texasExpertAgent = KnowledgeAugmentedPromptAgent(
+    openai_api_key = openai_api_key,
+    persona = persona,
+    knowledge = "You know everything about the history and geography of Texas and the Texas country in general."
+)
 
 knowledge = "You know everything about Europe"
 # TODO: 3 - Define the Europe Knowledge Augmented Prompt Agent
+europeExpertAgent = KnowledgeAugmentedPromptAgent(
+    openai_api_key = openai_api_key,
+    persona = persona,
+    knowledge = "You know everything about the history and geography of Europe and the European continent in general."
+)
 
 persona = "You are a college math professor"
 knowledge = "You know everything about math, you take prompts with numbers, extract math formulas, and show the answer without explanation"
 # TODO: 4 - Define the Math Knowledge Augmented Prompt Agent
+mathExpertAgent = KnowledgeAugmentedPromptAgent(
+    openai_api_key = openai_api_key,
+    persona = persona,
+    knowledge = "You know everything about the mathematics and any of its sub-domains and at any sophistication levels."
+)
 
 routing_agent = RoutingAgent(openai_api_key, {})
+
 agents = [
     {
         "name": "texas agent",
         "description": "Answer a question about Texas",
-        "func": lambda x: # TODO: 5 - Call the Texas Agent to respond to prompts
+        "func": lambda x: texasExpertAgent.respond(x) # TODO: 5 - Call the Texas Agent to respond to prompts
     },
     {
         "name": "europe agent",
         "description": "Answer a question about Europe",
-        "func": # TODO: 6 - Define a function to call the Europe Agent
+        "func": lambda x: europeExpertAgent.respond(x) # TODO: 6 - Define a function to call the Europe Agent
     },
     {
         "name": "math agent",
         "description": "When a prompt contains numbers, respond with a math formula",
-        # TODO: 7 - Define a function to call the Math Agent
+        "func": lambda x: mathExpertAgent.respond(x)  # TODO: 7 - Define a function to call the Math Agent
     }
 ]
 
@@ -45,3 +63,14 @@ routing_agent.agents = agents
 #           - "Tell me about the history of Rome, Texas"
 #           - "Tell me about the history of Rome, Italy"
 #           - "One story takes 2 days, and there are 20 stories"
+
+testPromptLs = [
+    "Tell me about the history of Rome, Texas",
+    "Tell me about the history of Rome, Italy",
+    "One story takes 2 days, and there are 20 stories"
+]
+
+for testPrompt in testPromptLs:
+    print(f"Test Prompt: {testPrompt}")
+    response = routing_agent.route(testPrompt)
+    print(f"\nFinal Response:\n{response}")
--- project/starter/phase_1/workflow_agents/base_agents.py
+++ project/starter/phase_1/workflow_agents/base_agents.py
@@ -321,36 +321,51 @@ class EvaluationAgent:
         }   
 
 
-'''
+
 class RoutingAgent():
 
     def __init__(self, openai_api_key, agents):
         # Initialize the agent with given attributes
         self.openai_api_key = openai_api_key
         # TODO: 1 - Define an attribute to hold the agents, call it agents
+        self.agents = agents
 
     def get_embedding(self, text):
-        client = OpenAI(api_key=self.openai_api_key)
+        client = OpenAI(
+            base_url = "https://openai.vocareum.com/v1",
+            api_key = self.openai_api_key
+        )
+
         # TODO: 2 - Write code to calculate the embedding of the text using the text-embedding-3-large model
         # Extract and return the embedding vector from the response
-        embedding = response.data[0].embedding
+        embeddingResponse = client.embeddings.create(
+                                                        input = text,
+                                                        model = "text-embedding-3-large",
+                                                    )
+
+        embedding = embeddingResponse.data[0].embedding
         return embedding 
 
     # TODO: 3 - Define a method to route user prompts to the appropriate agent
+    def route(self, user_input):
         # TODO: 4 - Compute the embedding of the user input prompt
-        input_emb = 
+        inputEmbed = self.get_embedding(user_input)
         best_agent = None
         best_score = -1
 
         for agent in self.agents:
             # TODO: 5 - Compute the embedding of the agent description
-            if agent_emb is None:
+            agentEmbed = self.get_embedding(agent["description"])
+            if agentEmbed is None:
                 continue
 
-            similarity = np.dot(input_emb, agent_emb) / (np.linalg.norm(input_emb) * np.linalg.norm(agent_emb))
+            similarity = np.dot(inputEmbed, agentEmbed) / (np.linalg.norm(inputEmbed) * np.linalg.norm(agentEmbed))
             print(similarity)
 
             # TODO: 6 - Add logic to select the best agent based on the similarity score between the user prompt and the agent descriptions
+            if similarity > best_score:
+                best_score = similarity
+                best_agent = agent
 
         if best_agent is None:
             return "Sorry, no suitable agent could be selected."
@@ -358,7 +373,6 @@ class RoutingAgent():
         print(f"[Router] Best agent: {best_agent['name']} (score={best_score:.3f})")
         return best_agent["func"](user_input)
 
-'''
 
 '''
 class ActionPlanningAgent:
